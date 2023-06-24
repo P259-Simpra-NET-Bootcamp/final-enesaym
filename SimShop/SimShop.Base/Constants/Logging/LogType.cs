c315b63f0c87a8922ef3f4f7c326cce00c5a2c96
Muhammet Enes AY <enesaym@gmail.com>
2023-06-24 23:15:48 +0300

    log fail fixed

---
+++ SimShop/SimShop.Base/Constants/Logging/LogType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimShop.Base.Constants;
+
+public static class LogType
+{
+    public static string LogedIn = "LogedIn";
+    public static string WrongPassword = "WrongPass";
+    public static string InValidUserName = "InValidUserName";
+    public static string RequestNull = "Request Null";
+    public static string DeleteError = "Delete Exception";
+    public static string GetError = "Get Exception";
+    public static string InsertError = "Insert Exception";
+    public static string UpdateError = "Update Exception";
+
+}
